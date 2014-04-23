f2ea7b9909833c52002198eeba1ff9d7ee112196
LuoGuo <luo_guo@icloud.com>
2014-04-23 17:17:09 +0800

    MessageProcessor类增加

---
+++ framework/org.tinygroup.message/src/main/java/org/tinygroup/message/MessageProcessor.java
@@ -0,0 +1,23 @@
+package org.tinygroup.message;
+
+
+/**
+ * 信息处理器
+ * Created by luoguo on 2014/4/23.
+ */
+public interface MessageProcessor {
+    /**
+     * 返回是否是匹配的消息
+     *
+     * @param message
+     * @return
+     */
+    boolean isMatch(MessageSender messageSender,MessageReceiver messageReceiver,Message message);
+
+    /**
+     * 进行处理
+     *
+     * @param message
+     */
+    void processMessage(MessageSender messageSender,MessageReceiver messageReceiver,Message message);
+}
