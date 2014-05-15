7a307920f1a138b295d79b11a73138101f8453d4
LuoGuo <luo_guo@icloud.com>
2014-05-15 11:07:38 +0800

    添加beanContainer工程

---
+++ framework/org.tinygroup.beancontainer/src/main/java/org/tinygroup/springutil/BeanContainer.java
@@ -0,0 +1,68 @@
+package org.tinygroup.springutil;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by luoguo on 2014/5/15.
+ */
+public interface BeanContainer<C> {
+    /**
+     * 返回原生的Bean窗口类型
+     *
+     * @return
+     */
+    C getBeanContainerPrototype();
+
+    /**
+     * 添加子容器
+     *
+     * @param subBeanContainer
+     */
+    void addSubBeanContainer(C subBeanContainer);
+
+    /**
+     * 返回子窗口列表
+     *
+     * @return
+     */
+    List<C> getSubBeanContainers();
+
+    /**
+     * 返回指定类型的bean列表
+     *
+     * @param type
+     * @param <T>
+     * @return
+     */
+    <T> Collection<T> getBeans(Class<T> type);
+
+    /**
+     * 获取指定名称的Bean
+     *
+     * @param name
+     * @param <T>
+     * @return
+     */
+    <T> T getBean(String name);
+
+    /**
+     * 获取指定类型的Bean
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    <T> T getBean(Class<T> clazz);
+
+    /**
+     * 获取指定类型指定名称的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    <T> T getBean(String name, Class<T> clazz);
+
+}
