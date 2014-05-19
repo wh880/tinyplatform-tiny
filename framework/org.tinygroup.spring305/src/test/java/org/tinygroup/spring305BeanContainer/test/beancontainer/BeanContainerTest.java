f312b7de0e6846db6d6218cca96bae86acd5c8e1
atizose <279708573@qq.com>
2014-05-19 16:58:07 +0800

    添加beancontainer的实现：spring305工程

---
+++ framework/org.tinygroup.spring305/src/test/java/org/tinygroup/spring305BeanContainer/test/beancontainer/BeanContainerTest.java
@@ -0,0 +1,25 @@
+package org.tinygroup.spring305BeanContainer.test.beancontainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.tinygroup.spring305BeanContainer.SpringBeanContainer;
+import org.tinygroup.vfs.FileObject;
+import org.tinygroup.vfs.VFS;
+
+public class BeanContainerTest extends TestCase {
+
+	public void testContainer() {
+		SpringBeanContainer sbc = new SpringBeanContainer();
+		FileObject f = VFS.resolveURL(this.getClass().getClassLoader().getResource("beancontainer.beans.xml"));
+		List<FileObject> fl = new ArrayList<FileObject>();
+		fl.add(f);
+		sbc.regSpringConfigXml(fl);
+		sbc.refresh();
+		ContainerBean bean = (ContainerBean) sbc.getBean("containerbean");
+		assertNotNull(bean);
+	}
+
+}
