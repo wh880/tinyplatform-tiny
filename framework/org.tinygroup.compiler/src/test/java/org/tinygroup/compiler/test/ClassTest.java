6e602e698db8f2b9c037afd9195e5d3ca3972019
atizose <279708573@qq.com>
2014-05-23 16:45:47 +0800

    测试用例

--- framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/ClassTest.java
+++ framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/ClassTest.java
@@ -1,19 +1,26 @@
 package org.tinygroup.compiler.test;
+
 import org.eclipse.jdt.core.compiler.CompilationProgress;
 import org.eclipse.jdt.core.compiler.batch.BatchCompiler;
 
 import java.io.PrintWriter;
 
+import junit.framework.TestCase;
+
 /**
  * Created by luoguo on 2014/5/21.
  */
-public class ClassTest {
-    public static void main(String[] args) {
-        CompilationProgress progress = null; // instantiate your subclass
-        BatchCompiler.compile(
-                "-classpath rt.jar E:\\test\\test\\Hello1.java E:\\test\\test\\Hello2.java",
-                new PrintWriter(System.out),
-                new PrintWriter(System.err),
-                progress);
-    }
+public class ClassTest extends TestCase {
+	public void test() {
+		CompilationProgress progress = null; // instantiate your subclass
+		String path = System.getProperty("user.dir");
+		String filePath = path + "\\test1\\org\\tinygroup\\Grade.java";
+		String filePath1 = path
+				+ "\\test1\\org\\tinygroup\\MyTestInterface.java";
+		boolean flag = BatchCompiler.compile(
+				"-classpath rt.jar " + filePath + " " + filePath1,
+				new PrintWriter(System.out), new PrintWriter(System.err),
+				progress);
+		assertTrue(flag);
+	}
 }
