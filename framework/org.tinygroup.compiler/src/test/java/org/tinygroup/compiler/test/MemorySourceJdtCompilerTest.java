8ef6e9f9e9acfba6e953769b353dc47645db003d
atizose <279708573@qq.com>
2014-05-23 14:37:16 +0800

    提交测试用例

--- framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/MemorySourceJdtCompilerTest.java
+++ framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/MemorySourceJdtCompilerTest.java
@@ -1,7 +1,6 @@
 package org.tinygroup.compiler.test;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
---
+++ framework/org.tinygroup.compiler/test/org/tinygroup/compiler/test/Abc.java
@@ -0,0 +1,19 @@
+package org.tinygroup.compiler.test;
+
+public class Abc {
+	private String name;
+	private int num;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
