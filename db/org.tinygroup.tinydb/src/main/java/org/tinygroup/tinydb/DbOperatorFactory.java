1b4d35790d85c47f92d1c2f071e04ed1ac8ab500
LuoGuo <luo_guo@icloud.com>
2014-05-23 08:07:34 +0800

    添加一个工厂类

---
+++ db/org.tinygroup.tinydb/src/main/java/org/tinygroup/tinydb/DbOperatorFactory.java
@@ -0,0 +1,17 @@
+package org.tinygroup.tinydb;
+
+import org.tinygroup.springutil.SpringUtil;
+import org.tinygroup.tinydb.operator.DBOperator;
+
+/**
+ * Created by luoguo on 2014/5/23.
+ */
+public final class DbOperatorFactory {
+    private DbOperatorFactory() {
+    }
+
+    static DBOperator getDBOperator(String beanType) {
+        BeanOperatorManager beanOperatorManager = (BeanOperatorManager) SpringUtil.getBean(BeanOperatorManager.OPERATOR_MANAGER_BEAN);
+        return beanOperatorManager.getDbOperator(beanType);
+    }
+}
