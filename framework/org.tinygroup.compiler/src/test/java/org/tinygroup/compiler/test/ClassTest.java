17b1c2dbac0dd5fa66e34d34954d9e8a18d7e8a9
LuoGuo <luo_guo@icloud.com>
2014-05-24 09:56:46 +0800

    修身ClassTest中测试用例的问题

--- framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/ClassTest.java
+++ framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/ClassTest.java
@@ -1,26 +1,26 @@
 package org.tinygroup.compiler.test;
 
+import junit.framework.TestCase;
 import org.eclipse.jdt.core.compiler.CompilationProgress;
 import org.eclipse.jdt.core.compiler.batch.BatchCompiler;
 
+import java.io.File;
 import java.io.PrintWriter;
-
-import junit.framework.TestCase;
 
 /**
  * Created by luoguo on 2014/5/21.
  */
 public class ClassTest extends TestCase {
-	public void test() {
-		CompilationProgress progress = null; // instantiate your subclass
-		String path = System.getProperty("user.dir");
-		String filePath = path + "\\test1\\org\\tinygroup\\Grade.java";
-		String filePath1 = path
-				+ "\\test1\\org\\tinygroup\\MyTestInterface.java";
-		boolean flag = BatchCompiler.compile(
-				"-classpath rt.jar " + filePath + " " + filePath1,
-				new PrintWriter(System.out), new PrintWriter(System.err),
-				progress);
-		assertTrue(flag);
-	}
+    public void test() {
+        CompilationProgress progress = null; // instantiate your subclass
+        String path = System.getProperty("user.dir");
+        String filePath = path + File.separatorChar + "test1" + File.separatorChar + "org" + File.separatorChar + "tinygroup" + File.separatorChar + "Grade.java";
+        String filePath1 = path
+                + "" + File.separatorChar + "test1" + File.separatorChar + "org" + File.separatorChar + "tinygroup" + File.separatorChar + "MyTestInterface.java";
+        boolean flag = BatchCompiler.compile(
+                "-classpath rt.jar " + filePath + " " + filePath1,
+                new PrintWriter(System.out), new PrintWriter(System.err),
+                progress);
+        assertTrue(flag);
+    }
 }
