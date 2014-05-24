3e2b29b17ba462ddeb0464e2c135b3ca604e14a3
LuoGuo <luo_guo@icloud.com>
2014-05-24 09:53:44 +0800

    修正测试用例中目录的书写格式

--- framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/MemorySourceJdtCompilerTest.java
+++ framework/org.tinygroup.compiler/src/test/java/org/tinygroup/compiler/test/MemorySourceJdtCompilerTest.java
@@ -11,25 +11,25 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
-public class MemorySourceJdtCompilerTest extends TestCase{
-	public void testCompile() {
-		
-		String path = System.getProperty("user.dir");
-		String filePath = path+ File.separatorChar+"test1"+File.separatorChar+"org\"+File.separatorChar+\"tinygroup\"+File.separatorChar+\"Grade.java";
-		String sourceFolder = path+"\"+File.separatorChar+\"test2";
-		String content = selfReadFile(filePath);
-		MemorySource source = new MemorySource("org.tinygroup.Grade", content);
-		MemorySourceJdtCompiler msc = new MemorySourceJdtCompiler(sourceFolder);
-		try {
-			boolean flag = msc.compile(source);
-			assertTrue(flag);
-		} catch (CompileException e) {
-			assertTrue(false);
-		}
-		
-	}
-	
-	private String selfReadFile(String strFileName) {
+public class MemorySourceJdtCompilerTest extends TestCase {
+    public void testCompile() {
+
+        String path = System.getProperty("user.dir");
+        String filePath = path + File.separatorChar + "test1" + File.separatorChar + "org" + File.separatorChar + "tinygroup" + File.separatorChar + "Grade.java";
+        String sourceFolder = path + File.separatorChar + "test2";
+        String content = selfReadFile(filePath);
+        MemorySource source = new MemorySource("org.tinygroup.Grade", content);
+        MemorySourceJdtCompiler msc = new MemorySourceJdtCompiler(sourceFolder);
+        try {
+            boolean flag = msc.compile(source);
+            assertTrue(flag);
+        } catch (CompileException e) {
+            assertTrue(false);
+        }
+
+    }
+
+    private String selfReadFile(String strFileName) {
         //Read the file into a string buffer, then return as a string.
         StringBuffer buf = null;//the intermediary, mutable buffer
         BufferedReader breader = null;//reader for the template files
