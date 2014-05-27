fe135991edc2cfb995e7134b7d8aaf93f50a6aa4
LuoGuo <luo_guo@icloud.com>
2014-05-27 10:00:15 +0800

    去掉sql中的license信息

--- framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/tools/ExceptionUtil.java
+++ framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/tools/ExceptionUtil.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.tinygroup.commons.tools;
 
 import static org.tinygroup.commons.tools.Assert.assertNotNull;
