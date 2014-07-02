e99b3cbdab9bf79d2c20760f3d9fbec4e60c8389
atizose <279708573@qq.com>
2014-07-02 15:35:07 +0800

    修改引用的配置文件

--- framework/org.tinygroup.cepcorepcsc/src/test/java/org/tinygroup/cepcorepcsc/test/service/TestServiceSc.java
+++ framework/org.tinygroup.cepcorepcsc/src/test/java/org/tinygroup/cepcorepcsc/test/service/TestServiceSc.java
@@ -8,7 +8,7 @@ import org.tinygroup.tinytestutil.AbstractTestUtil;
 
 public class TestServiceSc {
 	public static void main(String[] args) {
-		AbstractTestUtil.init("application.xml", true);
+		AbstractTestUtil.init("applicationsc.xml", true);
 		PcCepCoreImpl p  = SpringUtil.getBean(CEPCore.CEP_CORE_BEAN);
 		p.setNodeName("sc");
 		ScOperator o = new  ScOperator("192.168.84.23",8888);
