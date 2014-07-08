c3d9703f33d04bd15849b96efeca8e1b6b8107d3
atizose <279708573@qq.com>
2014-07-08 10:27:40 +0800

    删除多余方法

--- framework/org.tinygroup.bundle/src/test/java/org/tinygroup/bundle/test/manager/BundleManagerTest.java
+++ framework/org.tinygroup.bundle/src/test/java/org/tinygroup/bundle/test/manager/BundleManagerTest.java
@@ -43,38 +43,6 @@ public class BundleManagerTest extends TestCase{
 	
 	
 	
-	public void testLoad() {
-		TestUtil.init();
-		BundleManager manager = SpringUtil.getBean(BundleManager.BEAN_NAME);
-		BundleDefine b;
-		try {
-			b = manager.getBundleDefine("test1");
-			assertNotNull(b);
-		} catch (BundleException e) {
-			e.printStackTrace();
-			assertFalse(true);
-		}
-//		manager.stop();
-	}
-	public void testRemove(){
-		TestUtil.init();
-		BundleManager manager = SpringUtil.getBean(BundleManager.BEAN_NAME);
-		manager.start();
-		try {
-			manager.getTinyClassLoader("test2").loadClass("org.tinygroup.MyTestImpl2");
-			//manager.getTinyClassLoader().loadClass("org.tinygroup.hello.HelloImpl2");
-			
-		} catch (ClassNotFoundException e) {
-			assertFalse(true);
-		}
-		try {
-			manager.removeBundle(manager.getBundleDefine("test1"));
-		} catch (BundleException e) {
-			
-			e.printStackTrace();
-		}
-//		manager.stop();
-	}
-	
+
 
 }
