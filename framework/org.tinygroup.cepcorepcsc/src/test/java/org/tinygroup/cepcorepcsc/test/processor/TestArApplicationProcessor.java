08919b4455b06d765eb3c3c35e8e4779ce530b75
atizose <279708573@qq.com>
2014-07-09 10:03:07 +0800

    移除cepcorepcsc

--- framework/org.tinygroup.cepcorepcsc/src/test/java/org/tinygroup/cepcorepcsc/test/processor/TestArApplicationProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.tinygroup.cepcorepcsc.test.processor;
-
-import org.tinygroup.tinytestutil.AbstractTestUtil;
-
-public class TestArApplicationProcessor {
-	public static void main(String[] args) {
-		AbstractTestUtil.init("application.xml", true);
-	}
-}
