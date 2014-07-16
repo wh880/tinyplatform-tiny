a8e005f0826b61d62f6ec835957d3eb6d6c2d3c1
LuoGuo <luo_guo@icloud.com>
2014-07-16 16:11:24 +0800

    增加注释

--- framework/org.tinygroup.service/src/main/java/org/tinygroup/service/ServiceInterface.java
+++ framework/org.tinygroup.service/src/main/java/org/tinygroup/service/ServiceInterface.java
@@ -1,12 +1,31 @@
 package org.tinygroup.service;
 
-
+/**
+ * 服务接口
+ * 通过实现此接口来编写服务
+ * 通时有一个默认的约定：
+ * 在这个接口的实现类中，必须有一个execute方法，execute方法用于完成服务的业务逻辑实现，其入参及返回值必须实现序列化接口
+ */
 public interface ServiceInterface {
-	
-	String getServiceId();
-	
-	String getCategory();
-	
-	String getResultKey();
+    /**
+     * 返回服务ID
+     *
+     * @return
+     */
+    String getServiceId();
+
+    /**
+     * 返回目录，多重目录用“.”分隔
+     *
+     * @return
+     */
+    String getCategory();
+
+    /**
+     * 返回结果在上下文中的Key值
+     *
+     * @return
+     */
+    String getResultKey();
 
 }
