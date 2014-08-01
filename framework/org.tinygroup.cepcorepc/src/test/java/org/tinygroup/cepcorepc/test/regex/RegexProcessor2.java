36068ad8896ac3d7c61c6a26982346e078e2383a
atizose <279708573@qq.com>
2014-08-01 17:19:30 +0800

    补充提交之前遗漏的测试用例类

---
+++ framework/org.tinygroup.cepcorepc/src/test/java/org/tinygroup/cepcorepc/test/regex/RegexProcessor2.java
@@ -0,0 +1,50 @@
+package org.tinygroup.cepcorepc.test.regex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tinygroup.cepcore.CEPCore;
+import org.tinygroup.cepcore.EventProcessor;
+import org.tinygroup.cepcorepc.test.synchronous.Service;
+import org.tinygroup.cepcorepc.test.synchronous.SynchronousProcessor;
+import org.tinygroup.event.Event;
+import org.tinygroup.event.ServiceInfo;
+
+public class RegexProcessor2 implements EventProcessor{
+	List<ServiceInfo> list = new ArrayList<ServiceInfo>();
+	public void process(Event event) {
+	
+		System.out.println("RegexProcessor processor2");
+	}
+
+	public void setCepCore(CEPCore cepCore) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<ServiceInfo> getServiceInfos() {
+		return list;
+	}
+
+	public String getId() {
+		return SynchronousProcessor.class.getName();
+	}
+
+	public int getType() {
+		return EventProcessor.TYPE_LOCAL;
+	}
+
+	public int getWeight() {
+		return 0;
+	}
+	
+	public void addService(Service s){
+		list.add(s);
+	}
+
+	public List<String> getRegex() {
+		List<String> list = new ArrayList<String>();
+		list.add("(.)*");
+		return list;
+	}
+}
