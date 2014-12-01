064b2c8ce1629afdb74d9f04a497f13b00f276db
atizose <279708573@qq.com>
2014-12-01 16:20:11 +0800

    添加一句注释

--- framework/org.tinygroup.application/src/main/java/org/tinygroup/application/ApplicationProcessor.java
+++ framework/org.tinygroup.application/src/main/java/org/tinygroup/application/ApplicationProcessor.java
@@ -18,25 +18,27 @@ package org.tinygroup.application;
 import org.tinygroup.commons.order.Ordered;
 import org.tinygroup.config.Configuration;
 
-
 public interface ApplicationProcessor extends Configuration, Ordered {
-    /**
-     * 应用程序处理器开启方法
-     */
-    void start();
+	/**
+	 * 应用程序处理器开启方法
+	 */
+	void start();
 
-    void init();
+	/**
+	 * 应用程序处理器初始化方法
+	 */
+	void init();
 
-    /**
-     * 应用程序处理器关闭方法
-     */
-    void stop();
+	/**
+	 * 应用程序处理器关闭方法
+	 */
+	void stop();
 
-    void setApplication(Application application);
-    /**
-     * 设置本应用处理器所属的应用程序
-     *
-     * @param application
-     */
+	void setApplication(Application application);
+	/**
+	 * 设置本应用处理器所属的应用程序
+	 * 
+	 * @param application
+	 */
 
 }
