d1be7310be134d5ad5d82fbd179fb31ae03aa6db
aotianji1238 <aotianji1238@163.com>
2015-03-05 17:29:22 +0800

    处理#issue469，增加数据库语言类型

---
+++ db/org.tinygroup.metadata/src/main/java/org/tinygroup/metadata/config/stddatatype/LanguageType.java
@@ -0,0 +1,15 @@
+package org.tinygroup.metadata.config.stddatatype;
+
+import org.tinygroup.metadata.config.BaseObject;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * 数据库语言
+ * @author yancheng11334
+ *
+ */
+@XStreamAlias("language-type")
+public class LanguageType extends BaseObject{
+
+}
