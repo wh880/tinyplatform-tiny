b89516d07668d34284cb53f603fdf149ecdbfbfe
LuoGuo <luo_guo@icloud.com>
2015-03-05 14:31:47 +0800

    重构

--- framework/org.tinygroup.context2object/src/main/java/org/tinygroup/context2object/impl/DateTypeConverter.java
+++ framework/org.tinygroup.context2object/src/main/java/org/tinygroup/context2object/impl/DateTypeConverter.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
+ *  Copyright (c) 1997-2013, tinygroup.org (luo_guo@live.cn).
  *
  *  Licensed under the GPL, Version 3.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,70 +12,52 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ * --------------------------------------------------------------------------
+ *  版权 (c) 1997-2013, tinygroup.org (luo_guo@live.cn).
+ *
+ *  本开源软件遵循 GPL 3.0 协议;
+ *  如果您不遵循此协议，则不被允许使用此文件。
+ *  你可以从下面的地址获取完整的协议文本
+ *
+ *       http://www.gnu.org/licenses/gpl.html
  */
 package org.tinygroup.context2object.impl;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.tinygroup.context2object.TypeConverter;
 
 public class DateTypeConverter implements TypeConverter<String, Date> {
-	String format = "yyyy-MM-dd HH:mm:ss";
-	SimpleDateFormat dateFormat = null;
+    String format = "yyyy-MM-dd HH:mm:ss";
+    SimpleDateFormat dateFormat = null;
 
-	public String getFormat() {
-		return format;
-	}
+    public String getFormat() {
+        return format;
+    }
 
-	public void setFormat(String format) {
-		this.format = format;
-		dateFormat = new SimpleDateFormat(format);
-	}
+    public void setFormat(String format) {
+        this.format = format;
+        dateFormat = new SimpleDateFormat(format);
+    }
 
-	public Class<String> getSourceType() {
-		return String.class;
-	}
+    public Class<String> getSourceType() {
+        return String.class;
+    }
 
-	public Class<Date> getDestinationType() {
-		return Date.class;
-	}
+    public Class<Date> getDestinationType() {
+        return Date.class;
+    }
 
-	public Date getObject(String value) {
-
-        Pattern patternByEn = Pattern.compile("\\d{1,4}[-]\\d{1,2}[-]\\d{1,2}",
-                Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-        Pattern patternByZh = Pattern.compile("\\d{1,4}[年]\\d{1,2}[月]\\d{1,2}[日]",
-                Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-        Pattern patternByEnTimestamp = Pattern.compile("\\d{1,4}[-]\\d{1,2}[-]\\d{1,2}(\\s)*\\d{1,2}(:)\\d{1,2}(:)\\d{1,2}",
-                Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-        Pattern patternByZhTimestamp = Pattern.compile("\\d{1,4}[年]\\d{1,2}[月]\\d{1,2}[日](\\s)*\\d{1,2}(:)\\d{1,2}(:)\\d{1,2}",
-                Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-
-        Map<String, Pattern> rexMap = new HashMap<String, Pattern>();
-        rexMap.put("yyyy-MM-dd", patternByEn);
-        rexMap.put("yyyy年MM月dd日", patternByZh);
-        rexMap.put("yyyy-MM-dd HH:mm:ss", patternByEnTimestamp);
-        rexMap.put("yyyy年MM月dd日 HH:mm:ss", patternByZhTimestamp);
+    public Date getObject(String value) {
         if (dateFormat == null) {
             dateFormat = new SimpleDateFormat(format);
         }
         try {
-            for (String pattern : rexMap.keySet()) {
-                Matcher matcher = rexMap.get(pattern).matcher(value);
-                if (matcher.matches()) {
-                    dateFormat = new SimpleDateFormat(pattern);
-                    break ;
-                }
-            }
             return dateFormat.parse(value);
-        }catch (ParseException e) {
-           throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
-	}
+    }
 }
