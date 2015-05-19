ede43f27bd87769813719e8f447146f2c1b2c869
ballackhui <ballackhui@gmail.com>
2015-05-19 10:38:01 +0800

    增加打印sql与参数信息

--- db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/SimpleDslSession.java
+++ db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/SimpleDslSession.java
@@ -7,6 +7,9 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.tinygroup.commons.tools.CollectionUtil;
 import org.tinygroup.jdbctemplatedslsession.rowmapper.SimpleRowMapperSelector;
+import org.tinygroup.logger.LogLevel;
+import org.tinygroup.logger.Logger;
+import org.tinygroup.logger.LoggerFactory;
 import org.tinygroup.tinysqldsl.ComplexSelect;
 import org.tinygroup.tinysqldsl.Delete;
 import org.tinygroup.tinysqldsl.DslSession;
@@ -24,6 +27,8 @@ public class SimpleDslSession implements DslSession {
 
 	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
 	private RowMapperSelector selector = new SimpleRowMapperSelector();
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(SimpleDslSession.class);
 
 	public SimpleDslSession(DataSource dataSource) {
 		jdbcTemplate.setDataSource(dataSource);
@@ -42,19 +47,29 @@ public class SimpleDslSession implements DslSession {
 	}
 
 	public int execute(Insert insert) {
+		logMessage(insert.sql(), insert.getValues());
 		return jdbcTemplate.update(insert.sql(), insert.getValues().toArray());
 	}
 
+	private void logMessage(String sql, List<Object> values) {
+		LOGGER.logMessage(LogLevel.DEBUG,
+				"Executing SQL:[{0}],values:{1}", sql,
+				values);
+	}
+
 	public int execute(Update update) {
+		logMessage(update.sql(), update.getValues());
 		return jdbcTemplate.update(update.sql(), update.getValues().toArray());
 	}
 
 	public int execute(Delete delete) {
+		logMessage(delete.sql(), delete.getValues());
 		return jdbcTemplate.update(delete.sql(), delete.getValues().toArray());
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> T fetchOneResult(Select select, Class<T> requiredType) {
+		logMessage(select.sql(), select.getValues());
 		return (T) jdbcTemplate.queryForObject(select.sql(), select.getValues()
 				.toArray(), selector.rowMapperSelector(requiredType));
 	}
@@ -70,6 +85,7 @@ public class SimpleDslSession implements DslSession {
 
 	@SuppressWarnings("unchecked")
 	public <T> List<T> fetchList(Select select, Class<T> requiredType) {
+		logMessage(select.sql(), select.getValues());
 		return jdbcTemplate.query(select.toString(), select.getValues()
 				.toArray(), selector.rowMapperSelector(requiredType));
 	}
@@ -86,6 +102,7 @@ public class SimpleDslSession implements DslSession {
 	@SuppressWarnings("unchecked")
 	public <T> List<T> fetchList(ComplexSelect complexSelect,
 			Class<T> requiredType) {
+		logMessage(complexSelect.sql(), complexSelect.getValues());
 		return jdbcTemplate.query(complexSelect.toString(), complexSelect
 				.getValues().toArray(), selector
 				.rowMapperSelector(requiredType));
@@ -94,6 +111,7 @@ public class SimpleDslSession implements DslSession {
 	@SuppressWarnings("unchecked")
 	public <T> T fetchOneResult(ComplexSelect complexSelect,
 			Class<T> requiredType) {
+		logMessage(complexSelect.sql(), complexSelect.getValues());
 		return (T) jdbcTemplate.queryForObject(complexSelect.sql(),
 				complexSelect.getValues().toArray(),
 				selector.rowMapperSelector(requiredType));
