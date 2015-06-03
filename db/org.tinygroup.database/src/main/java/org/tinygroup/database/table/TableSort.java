bacdf0ce7fd14b8fb5ad84ed5cc0985af0963762
ballackhui <ballackhui@gmail.com>
2015-06-03 10:46:01 +0800

    # 610 SQL生成，生成的外键，存在执行顺序的问题

--- db/org.tinygroup.database/src/main/java/org/tinygroup/database/table/TableSort.java
+++ db/org.tinygroup.database/src/main/java/org/tinygroup/database/table/TableSort.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import org.tinygroup.commons.tools.CollectionUtil;
 import org.tinygroup.database.config.table.ForeignReference;
 import org.tinygroup.database.config.table.Table;
 
@@ -28,20 +27,16 @@ public class TableSort implements Comparator<Table> {
 	public int compare(Table table1, Table table2) {
 		List<String> dependTables1 = getDependList(table1);
 		List<String> dependTables2 = getDependList(table2);
-		boolean isEmpty1 = CollectionUtil.isEmpty(dependTables1);
-		boolean isEmpty2 = CollectionUtil.isEmpty(dependTables2);
-		if (isEmpty1||isEmpty2) {
-			boolean contains1 = dependTables1.contains(table2.getName());
-			boolean contains2 = dependTables2.contains(table1.getName());
-			if (contains1 && contains2) {
-				throw new RuntimeException(String.format(
-						"表1[name:%s]与表2[name:%s]相互依赖", table1.getName(),
-						table2.getName()));
-			} else if (contains1) {
-				return 1;
-			} else if (contains2) {
-				return -1;
-			}
+		boolean contains1 = dependTables1.contains(table2.getId());
+		boolean contains2 = dependTables2.contains(table1.getId());
+		if (contains1 && contains2) {
+			throw new RuntimeException(String.format(
+					"表1[name:%s]与表2[name:%s]相互依赖", table1.getName(),
+					table2.getName()));
+		} else if (contains1) {
+			return 1;
+		} else if (contains2) {
+			return -1;
 		}
 		return 0;
 	}
@@ -54,7 +49,4 @@ public class TableSort implements Comparator<Table> {
 		}
 		return dependencies;
 	}
-	
 }
-
-
