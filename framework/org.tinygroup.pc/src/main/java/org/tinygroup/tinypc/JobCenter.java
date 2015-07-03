6e30e512c72c3d8dc0106073b14dabec6b2d3434
atizose <279708573@qq.com>
2015-07-03 16:31:39 +0800

    调整接口方法顺序，便于阅读理解

--- framework/org.tinygroup.pc/src/main/java/org/tinygroup/tinypc/JobCenter.java
+++ framework/org.tinygroup.pc/src/main/java/org/tinygroup/tinypc/JobCenter.java
@@ -22,129 +22,122 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 /**
- * 职介所
- * 职介所是分布式处理的核心场所，所有工作相关的元素都要通过职介所进行关联
- * Created by luoguo on 14-1-8.
+ * 职介所 职介所是分布式处理的核心场所，所有工作相关的元素都要通过职介所进行关联 Created by luoguo on 14-1-8.
  */
 public interface JobCenter {
-    String WORK_QUEUE = "WorkQueue";
-    String FOREMAN = "Foreman";
-    String WORKER = "Worker";
-    int DEFAULT_PORT = 3333;
+	String WORK_QUEUE = "WorkQueue";
+	String FOREMAN = "Foreman";
+	String WORKER = "Worker";
+	int DEFAULT_PORT = 3333;
 
-    RmiServer getRmiServer();
+	RmiServer getRmiServer();
 
-    void setRmiServer(RmiServer rmiServer) throws RemoteException;
+	void setRmiServer(RmiServer rmiServer) throws RemoteException;
 
-    /**
-     * 注册工人
-     *
-     * @param worker
-     */
-    void registerWorker(Worker worker) throws RemoteException;
-    
-    
+	/**
+	 * 注册工人
+	 * 
+	 * @param worker
+	 */
+	void registerWorker(Worker worker) throws RemoteException;
 
-    /**
-     * 返回工作队列对象
-     *
-     * @return
-     */
-    WorkQueue getWorkQueue();
+	/**
+	 * 注消工人
+	 * 
+	 * @param worker
+	 */
+	void unregisterWorker(Worker worker) throws RemoteException;
 
-    /**
-     * 注消工人
-     *
-     * @param worker
-     */
-    void unregisterWorker(Worker worker) throws RemoteException;
+	/**
+	 * 返回工作队列对象
+	 * 
+	 * @return
+	 */
+	WorkQueue getWorkQueue();
 
-    /**
-     * 注册一份工作，工作情况不需要马上关注。因此也就不用等待，马上返回可以进行其它处理
-     * 如果有返回结果，可以通过异步方式，，异步方式可以用后续工作的方式来指定
-     *
-     * @param work
-     */
-    void registerWork(Work work) throws IOException;
+	/**
+	 * 返回所有的工作列表
+	 * 
+	 * @return
+	 */
+	List<Work> getWorkList() throws RemoteException;
 
+	/**
+	 * 返回具有某种类型的空闲且愿意接受工作的工人列表
+	 * 
+	 * @return
+	 */
+	List<Worker> getWorkerList(Work work) throws RemoteException;
 
-    /**
-     * 取消工作，在工作没有分配出去之前，可以从职介所注消工作，如果工作已经分配出去，则无法注消
-     *
-     * @param work
-     */
-    void unregisterWork(Work work) throws RemoteException;
+	/**
+	 * 返回某种类型的某种状态的工作列表
+	 * 
+	 * @return
+	 */
+	List<Work> getWorkList(String type, WorkStatus workStatus)
+			throws RemoteException;
 
-    /**
-     * 返回指定工作的工作状态
-     *
-     * @param work
-     * @return
-     */
-    WorkStatus getWorkStatus(Work work) throws RemoteException;
+	/**
+	 * 注册一份工作，工作情况不需要马上关注。因此也就不用等待，马上返回可以进行其它处理
+	 * 如果有返回结果，可以通过异步方式，，异步方式可以用后续工作的方式来指定
+	 * 
+	 * @param work
+	 */
+	void registerWork(Work work) throws IOException;
 
-    /**
-     * 执行一项工作，期望同步得到结果或异常
-     * 如果没有合适的工人或包工头进行处理，马上会抛出异常
-     *
-     * @param work
-     */
-    Warehouse doWork(Work work) throws IOException;
+	/**
+	 * 取消工作，在工作没有分配出去之前，可以从职介所注消工作，如果工作已经分配出去，则无法注消
+	 * 
+	 * @param work
+	 */
+	void unregisterWork(Work work) throws RemoteException;
 
-    /**
-     * 注册包工头
-     *
-     * @param foreman
-     */
-    void registerForeman(Foreman foreman) throws RemoteException;
+	/**
+	 * 执行一项工作，期望同步得到结果或异常 如果没有合适的工人或包工头进行处理，马上会抛出异常
+	 * 
+	 * @param work
+	 */
+	Warehouse doWork(Work work) throws IOException;
 
-    /**
-     * 注销包工头
-     *
-     * @param foreman
-     */
-    void unregisterForeMan(Foreman foreman) throws RemoteException;
+	/**
+	 * 返回指定工作的工作状态
+	 * 
+	 * @param work
+	 * @return
+	 */
+	WorkStatus getWorkStatus(Work work) throws RemoteException;
 
-    /**
-     * 返回具有某种类型的空闲且愿意接受工作的工人列表
-     *
-     * @return
-     */
-    List<Worker> getWorkerList(Work work) throws RemoteException;
+	/**
+	 * 注册包工头
+	 * 
+	 * @param foreman
+	 */
+	void registerForeman(Foreman foreman) throws RemoteException;
 
+	/**
+	 * 注销包工头
+	 * 
+	 * @param foreman
+	 */
+	void unregisterForeMan(Foreman foreman) throws RemoteException;
 
-    /**
-     * 返回所有的工作列表
-     *
-     * @return
-     */
-    List<Work> getWorkList() throws RemoteException;
+	/**
+	 * 返回组织某种工作的的空闲工头列表
+	 * 
+	 * @param type
+	 * @return
+	 */
+	List<Foreman> getForeman(String type) throws RemoteException;
 
-    /**
-     * 返回某种类型的某种状态的工作列表
-     *
-     * @return
-     */
-    List<Work> getWorkList(String type, WorkStatus workStatus) throws RemoteException;
+	/**
+	 * 自动进行匹配，如果有匹配成功的，则予以触发执行
+	 */
+	void autoMatch() throws IOException;
 
-
-    /**
-     * 返回组织某种工作的的空闲工头列表
-     *
-     * @param type
-     * @return
-     */
-    List<Foreman> getForeman(String type) throws RemoteException;
-
-    /**
-     * 自动进行匹配，如果有匹配成功的，则予以触发执行
-     */
-    void autoMatch() throws IOException;
-
-    /**
-     * 职业介绍所关门
-     *
-     * @throws RemoteException
-     */
-    void stop() throws RemoteException;
+	/**
+	 * 职业介绍所关门
+	 * 
+	 * @throws RemoteException
+	 */
+	void stop() throws RemoteException;
 }
--- framework/org.tinygroup.pc/src/main/java/org/tinygroup/tinypc/WorkQueue.java
+++ framework/org.tinygroup.pc/src/main/java/org/tinygroup/tinypc/WorkQueue.java
@@ -25,103 +25,105 @@ import java.util.List;
  */
 public interface WorkQueue extends RemoteObject {
 
-    /**
-     * 设置对象仓库
-     *
-     * @param objectStorage
-     */
-    void setObjectStorage(ObjectStorage objectStorage) throws RemoteException;
+	/**
+	 * 设置对象仓库
+	 * 
+	 * @param objectStorage
+	 */
+	void setObjectStorage(ObjectStorage objectStorage) throws RemoteException;
 
-    /**
-     * 添加一个工作
-     *
-     * @param work
-     * @throws RemoteException
-     */
-    void add(Work work) throws RemoteException;
+	/**
+	 * 添加一个工作
+	 * 
+	 * @param work
+	 * @throws RemoteException
+	 */
+	void add(Work work) throws RemoteException;
 
-    /**
-     * 更新工作状态
-     *
-     * @param work
-     * @throws RemoteException
-     */
-    void updateWorkStatus(Work work, WorkStatus workStatus) throws RemoteException;
+	/**
+	 * 删除一个工作
+	 * 
+	 * @param work
+	 * @throws RemoteException
+	 */
 
-    /**
-     * 获取工作状态
-     *
-     * @param work
-     * @return
-     * @throws RemoteException
-     */
-    WorkStatus getWorkStatus(Work work) throws RemoteException;
+	void remove(Work work) throws RemoteException;
 
-    /**
-     * 删除一个工作
-     *
-     * @param work
-     * @throws RemoteException
-     */
+	/**
+	 * 更新工作状态
+	 * 
+	 * @param work
+	 * @throws RemoteException
+	 */
+	void updateWorkStatus(Work work, WorkStatus workStatus)
+			throws RemoteException;
 
-    void remove(Work work) throws RemoteException;
+	/**
+	 * 获取工作状态
+	 * 
+	 * @param work
+	 * @return
+	 * @throws RemoteException
+	 */
+	WorkStatus getWorkStatus(Work work) throws RemoteException;
 
-    /**
-     * 返回工作总数
-     *
-     * @return
-     */
-    int size() throws RemoteException;
+	/**
+	 * 返回工作总数
+	 * 
+	 * @return
+	 */
+	int size() throws RemoteException;
 
-    /**
-     * 返回所有的工作列表
-     *
-     * @return
-     * @throws RemoteException
-     */
-    List<Work> getWorkList() throws RemoteException;
+	/**
+	 * 返回所有的工作列表
+	 * 
+	 * @return
+	 * @throws RemoteException
+	 */
+	List<Work> getWorkList() throws RemoteException;
 
-    /**
-     * 返回一个空闲工作
-     *
-     * @return
-     * @throws RemoteException
-     */
-    Work getIdleWork() throws RemoteException;
+	/**
+	 * 返回一个空闲工作
+	 * 
+	 * @return
+	 * @throws RemoteException
+	 */
+	Work getIdleWork() throws RemoteException;
 
-    /**
-     * 返回指定类型指定工作状态的工作列表
-     *
-     * @param type
-     * @param workStatus
-     * @return
-     * @throws RemoteException
-     */
-    List<Work> getWorkList(String type, WorkStatus workStatus) throws RemoteException;
+	/**
+	 * 返回指定类型指定工作状态的工作列表
+	 * 
+	 * @param type
+	 * @param workStatus
+	 * @return
+	 * @throws RemoteException
+	 */
+	List<Work> getWorkList(String type, WorkStatus workStatus)
+			throws RemoteException;
 
-    /**
-     * 返回指定工作状态的工作列表
-     *
-     * @param workStatus
-     * @return
-     * @throws RemoteException
-     */
-    List<Work> getWorkList(WorkStatus workStatus) throws RemoteException;
+	/**
+	 * 返回指定工作状态的工作列表
+	 * 
+	 * @param workStatus
+	 * @return
+	 * @throws RemoteException
+	 */
+	List<Work> getWorkList(WorkStatus workStatus) throws RemoteException;
 
-    /**
-     * 去掉一个旧工作，添加一个新工作
-     *
-     * @param oldWork
-     * @param newWork
-     * @throws RemoteException
-     */
-    void replace(Work oldWork, Work newWork) throws RemoteException;
+	/**
+	 * 去掉一个旧工作，添加一个新工作
+	 * 
+	 * @param oldWork
+	 * @param newWork
+	 * @throws RemoteException
+	 */
+	void replace(Work oldWork, Work newWork) throws RemoteException;
 
-    /**
-     * 把工作移动到工作队列最后
-     *
-     * @param work
-     */
-    void moveToLast(Work work) throws RemoteException;
+	/**
+	 * 把工作移动到工作队列最后
+	 * 
+	 * @param work
+	 */
+	void moveToLast(Work work) throws RemoteException;
 
 }
