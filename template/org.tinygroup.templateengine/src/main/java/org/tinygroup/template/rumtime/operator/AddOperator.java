38aff4af05ed1e177451ad0954d329062a2d63fe
悠然 <luo_guo@icloud.com>
2015-08-03 11:10:22 +0800

    字符串+null时,null当空串

--- template/org.tinygroup.templateengine/src/main/java/org/tinygroup/template/rumtime/operator/AddOperator.java
+++ template/org.tinygroup.templateengine/src/main/java/org/tinygroup/template/rumtime/operator/AddOperator.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
- *
- *  Licensed under the GPL, Version 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.gnu.org/licenses/gpl.html
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
+ * <p/>
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.gnu.org/licenses/gpl.html
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.tinygroup.template.rumtime.operator;
 
@@ -24,27 +24,27 @@ public class AddOperator extends TwoConvertOperator {
 
 
     protected Object operation(Object left, Object right) {
-        if (isType(left, Byte.class)&&isType(right, Byte.class)) {
+        if (isType(left, Byte.class) && isType(right, Byte.class)) {
             return (Byte) left + (Byte) right;
         }
-        if (isType(left, Character.class)&&isType(right, Character.class)) {
+        if (isType(left, Character.class) && isType(right, Character.class)) {
             return (Character) left + (Character) right;
         }
-        if (isType(left, Integer.class)&&isType(right, Integer.class)) {
+        if (isType(left, Integer.class) && isType(right, Integer.class)) {
             return (Integer) left + (Integer) right;
         }
-        if (isType(left, Float.class)&&isType(right, Float.class)) {
+        if (isType(left, Float.class) && isType(right, Float.class)) {
             return (Float) left + (Float) right;
         }
-        if (isType(left, Double.class)&&isType(right, Double.class)) {
+        if (isType(left, Double.class) && isType(right, Double.class)) {
             return (Double) left + (Double) right;
         }
-        if (isType(left, BigDecimal.class)&&isType(right, BigDecimal.class)) {
+        if (isType(left, BigDecimal.class) && isType(right, BigDecimal.class)) {
             BigDecimal b1 = (BigDecimal) left;
             BigDecimal b2 = (BigDecimal) right;
             return b1.add(b2);
         }
-        return left.toString() + right.toString();
+        return (left == null ? left.toString() : "") + (right == null ? right.toString() : "");
     }
 
 
