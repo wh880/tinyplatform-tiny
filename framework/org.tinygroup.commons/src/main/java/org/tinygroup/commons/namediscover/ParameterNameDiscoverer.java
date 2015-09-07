cdc9fd813100c1fbe81a5ca75845062a0dc10a27
ballackhui <ballackhui@gmail.com>
2015-09-07 16:42:44 +0800

    # 762 注解方式配置服务存在参数解析错误的问题，导致org.tinygroup.tinyonlineweb页面访问出错

--- framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/namediscover/ParameterNameDiscoverer.java
+++ framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/namediscover/ParameterNameDiscoverer.java
@@ -13,22 +13,15 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.tinygroup.commons.tools;
+package org.tinygroup.commons.namediscover;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 /**
- * Interface to discover parameter names for methods and constructors.
+ * 
+ * @author renhui
  *
- * <p>Parameter name discovery is not always possible, but various strategies are
- * available to try, such as looking for debug information that may have been
- * emitted at compile time, and looking for argname annotation values optionally
- * accompanying AspectJ annotated methods.
- *
- * @author Rod Johnson
- * @author Adrian Colyer
- * @since 2.0
  */
 public interface ParameterNameDiscoverer {
 	
