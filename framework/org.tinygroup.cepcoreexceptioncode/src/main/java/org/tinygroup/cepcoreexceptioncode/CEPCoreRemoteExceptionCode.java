414ea4836d3818d9f083f9b814da987513cf9e01
atizose <279708573@qq.com>
2015-09-10 11:26:14 +0800

    添加工程cepcoreexceptioncode工程 存放cep相关的异常号

---
+++ framework/org.tinygroup.cepcoreexceptioncode/src/main/java/org/tinygroup/cepcoreexceptioncode/CEPCoreRemoteExceptionCode.java
@@ -0,0 +1,8 @@
+package org.tinygroup.cepcoreexceptioncode;
+
+public class CEPCoreRemoteExceptionCode {
+	private CEPCoreRemoteExceptionCode(){}
+	public static final String REQUEST_EXCEPTION_CODE = "0TE110108" + "001";
+	public static final String TIMEOUT_EXCEPTION_CODE = "0TE110108" + "002";
+	public static final String HAS_TIMEOUT_EXCEPTION_CODE = "0TE110108" + "003";
+}
