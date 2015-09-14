aa5747ffad86d25b3da6a68ceadc4d9a80c8ec13
atizose <279708573@qq.com>
2015-09-14 13:56:46 +0800

    添加服务执行时间统计功能

--- framework/org.tinygroup.cepcoregovernance/src/main/java/org/tinygroup/cepcoregovernance/container/CommonExecuteInfoContainer.java
+++ framework/org.tinygroup.cepcoregovernance/src/main/java/org/tinygroup/cepcoregovernance/container/CommonExecuteInfoContainer.java
@@ -19,9 +19,23 @@ public class CommonExecuteInfoContainer {
 	 * serviceId,成功次数
 	 */
 	private Map<String, Long> serviceSucessTimeMap = new HashMap<String, Long>();
+
+	/**
+	 * eventId,serviceId
+	 */
 	private Map<String, String> serviceInfo = new HashMap<String, String>();
 
-	// TODO:单个服务越界怎么办，所有服务之和越界怎么办
+	/**
+	 * eventId,beginTime
+	 * 
+	 */
+	private Map<String, Long> beginTimeInfo = new HashMap<String, Long>();
+
+	/**
+	 * service,ExecuteTimeInfo
+	 */
+	private Map<String, ExecuteTimeInfo> executeTimeInfoMap = new HashMap<String, ExecuteTimeInfo>();
+
 	public Long getTotalTimes() {
 		return getTotalValue(serviceTimeMap);
 	}
@@ -49,18 +63,36 @@ public class CommonExecuteInfoContainer {
 	public void addExecuteBefore(Event e) {
 		String serviceId = getServiceId(e);
 		serviceInfo.put(e.getEventId(), serviceId);
+		addTimeInfo(serviceId,e.getEventId());
 		addExecute(serviceId, serviceTimeMap);
+	}
+
+	private void addTimeInfo(String serviceId,String eventId) {
+		long beginTime = System.currentTimeMillis();
+		beginTimeInfo.put(eventId, beginTime);
+		if (!executeTimeInfoMap.containsKey(serviceId)) {
+			executeTimeInfoMap.put(serviceId, new ExecuteTimeInfo(serviceId));
+		}
 	}
 
 	public void addExecuteAfter(Event e) {
 		String serviceId = getServiceId(e);
 		serviceInfo.remove(e.getEventId());
+		updateTimeInfo(serviceId,e.getEventId());
 		addExecute(serviceId, serviceSucessTimeMap);
+	}
+
+	private void updateTimeInfo(String serviceId,String eventId) {
+		long afterTime = System.currentTimeMillis();
+		long beginTime = beginTimeInfo.remove(eventId);
+		long executeTime = afterTime - beginTime;
+		executeTimeInfoMap.get(serviceId).addTime(executeTime);
 	}
 
 	public void addExecuteException(Event e, Exception t) {
 		String serviceId = getServiceId(e);
 		serviceInfo.remove(e.getEventId());
+		updateTimeInfo(serviceId, e.getEventId());
 		addExecute(serviceId, serviceExceptionTimeMap);
 	}
 
@@ -84,4 +116,9 @@ public class CommonExecuteInfoContainer {
 		serviceSucessTimeMap.clear();
 		serviceExceptionTimeMap.clear();
 	}
+	
+	public ExecuteTimeInfo getServiceExecuteTimeInfo(String serviceId){
+		return executeTimeInfoMap.get(serviceId);
+	}
+	
 }
