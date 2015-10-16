7c4f4df3f4ef96df5ecec155ab12b6c06defb090
305702665@qq.com <305702665@qq.com>
2015-10-16 16:11:16 +0800

    单元测试修改

--- ext/org.tinygroup.rediscache/src/test/java/org/tinygroup/cache/redis/RedisCachePerformanceTest.java
+++ ext/org.tinygroup.rediscache/src/test/java/org/tinygroup/cache/redis/RedisCachePerformanceTest.java
@@ -9,18 +9,15 @@ import org.tinygroup.cache.Cache;
 import org.tinygroup.cache.CacheManager;
 import org.tinygroup.tinyrunner.Runner;
 
-import junit.framework.TestCase;
+import junit.framework.Assert;
 
 /**
  * 验证RedisCache的性能<br>
  * 具体的redis配置地址请修改sample.jedisconfig.xml
  */
-public class RedisCachePerformanceTest extends TestCase{
+public class RedisCachePerformanceTest{
 
-	/**
-	 * @param args
-	 */
-	public void testGetSet() {
+	public static void main(String[] args) {
         Runner.init(null, new ArrayList<String>());
 		CacheManager manager = BeanContainerFactory.getBeanContainer(RedisCacheTest.class.getClassLoader()).getBean("redisCacheManager");
 		Cache cache = manager.createCache("server01");
@@ -32,10 +29,10 @@ public class RedisCachePerformanceTest extends TestCase{
 		long time = System.currentTimeMillis();
 		for(int i=0;i<10000;i++){
 		    cache.put("food", foods.get("food1"));
-		    assertEquals(cache.get("food"),foods.get("food1"));
+		    Assert.assertEquals(cache.get("food"),foods.get("food1"));
 		    cache.remove("food");
 		}
 		System.out.println("cost time:"+(System.currentTimeMillis()-time)+"ms");
 	}
-
+	
 }
