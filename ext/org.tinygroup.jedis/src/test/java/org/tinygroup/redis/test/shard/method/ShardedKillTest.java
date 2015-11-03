2e2fed3690bce85be6eceba2955bca5c07801ac5
atizose <279708573@qq.com>
2015-11-03 10:54:36 +0800

    添加测试用例

--- ext/org.tinygroup.jedis/src/test/java/org/tinygroup/redis/test/shard/method/ShardedKillTest.java
+++ ext/org.tinygroup.jedis/src/test/java/org/tinygroup/redis/test/shard/method/ShardedKillTest.java
@@ -2,6 +2,8 @@ package org.tinygroup.redis.test.shard.method;
 
 import java.util.ArrayList;
 
+import junit.framework.Assert;
+
 import org.tinygroup.beancontainer.BeanContainerFactory;
 import org.tinygroup.jedis.ShardJedisSentinelManager;
 import org.tinygroup.jedis.shard.TinyShardJedis;
@@ -9,9 +11,10 @@ import org.tinygroup.redis.test.RedisTest;
 import org.tinygroup.redis.test.shard.ShardJedisSentinelManagerImplTest;
 import org.tinygroup.tinyrunner.Runner;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPoolConfig;
 
-public class ShardedWriteTest {
+public class ShardedKillTest {
 	public static void main(String[] args) {
 		Runner.init("application.xml", new ArrayList<String>());
 		JedisPoolConfig jedisConfig = BeanContainerFactory.getBeanContainer(
@@ -23,16 +26,31 @@ public class ShardedWriteTest {
 		manager.init(jedisConfig);
 		TinyShardJedis jedis = manager.getShardedJedis();
 		System.out.println("读从:");
-		jedis.getReadShard("aaa").get("a");
+		jedis.del("aaa");
+		Assert.assertTrue(!jedis.exists("aaa"));
+		jedis.set("aaa", "aaa");
+
+		Jedis writeShard = jedis.getShard("aaa");
+		Jedis readShard = jedis.getReadShard("aaa");
+		readShard.get("a");
 		System.out.println("写从:");
 		try {
-			jedis.getReadShard("aaa").set("a", "b");
+			readShard.set("a", "b");
 			System.out.println("写从未发生异常:错误！");
+			Assert.assertTrue(false);
 		} catch (Exception e) {
 			System.out.println("写从发生异常:正确");
+			Assert.assertTrue(true);
 		}
 		manager.returnResource(jedis);
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		TinyShardJedis jedis2 = manager.getShardedJedis();
+		jedis2.set("aaa", "aaa");
 		manager.destroy();
-		
+
 	}
 }
