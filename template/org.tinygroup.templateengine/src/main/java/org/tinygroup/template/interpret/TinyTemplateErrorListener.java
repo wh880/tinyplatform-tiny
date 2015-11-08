c3d8d51213466ee3e3045b5031989e54c89cdd6e
aotianji1238 <aotianji1238@163.com>
2015-11-08 21:56:20 +0800

    处理模板引擎重构导致Eclipse工具丢失错误信息的情况

--- template/org.tinygroup.templateengine/src/main/java/org/tinygroup/template/interpret/TinyTemplateErrorListener.java
+++ template/org.tinygroup.templateengine/src/main/java/org/tinygroup/template/interpret/TinyTemplateErrorListener.java
@@ -1,15 +1,17 @@
 package org.tinygroup.template.interpret;
 
+import java.util.BitSet;
+
 import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.misc.Nullable;
 import org.tinygroup.template.TemplateException;
-
-import java.util.BitSet;
 
 /**
  * Created by luoguo on 15/8/11.
@@ -21,7 +23,13 @@ public class TinyTemplateErrorListener implements org.antlr.v4.runtime.ANTLRErro
         this.fileName=fileName;
     }
     public void syntaxError(@NotNull Recognizer<?, ?> recognizer, @Nullable Object o, int i, int i1, @NotNull String s, @Nullable RecognitionException e) {
-        exception=new TemplateException(e, null,fileName);
+    	RuleContext ruleContext=e.getCtx();
+    	if(ruleContext!=null && ruleContext instanceof ParserRuleContext){
+    		exception=new TemplateException(e,(ParserRuleContext)ruleContext,fileName);
+    	}else{
+    		exception=new TemplateException(e, null,fileName);
+    	}
+        
     }
 
     public void reportAmbiguity(@NotNull Parser parser, @NotNull DFA dfa, int i, int i1, boolean b, @Nullable BitSet bitSet, @NotNull ATNConfigSet atnConfigSet) {
