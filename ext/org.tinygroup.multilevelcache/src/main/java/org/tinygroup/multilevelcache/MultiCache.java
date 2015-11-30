5ac4b5a1f89ffdd25c37bfc7427ce2bec6cee628
305702665@qq.com <305702665@qq.com>
2015-11-30 16:07:33 +0800

    多级缓存代码提交

--- ext/org.tinygroup.multilevelcache/src/main/java/org/tinygroup/multilevelcache/MultiCache.java
+++ ext/org.tinygroup.multilevelcache/src/main/java/org/tinygroup/multilevelcache/MultiCache.java
@@ -4,12 +4,12 @@
 package org.tinygroup.multilevelcache;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.tinygroup.cache.Cache;
 import org.tinygroup.cache.CacheManager;
-import org.tinygroup.cache.exception.CacheException;
 
 /**
  * @author Administrator
@@ -17,48 +17,42 @@ import org.tinygroup.cache.exception.CacheException;
  */
 public class MultiCache implements Cache{
 
-	Cache cache1;
+	/**
+	 * 1级缓存
+	 */
+	Cache cache_first;
 	
-	Cache cache2;
-	
-	private CacheManager cacheManager;
+	/**
+	 * 2级缓存
+	 */
+	Cache cache_second;
 	
 	public MultiCache() {
 	}
 	
-	public MultiCache(Cache cache1, Cache cache2) {
+	public MultiCache(Cache cache_first, Cache cache2_second) {
 		super();
-		this.cache1 = cache1;
-		this.cache2 = cache2;
-	}
-
-	public void setCache1(Cache cache1) {
-		this.cache1 = cache1;
-	}
-
-	public void setCache2(Cache cache2) {
-		this.cache2 = cache2;
+		this.cache_first = cache_first;
+		this.cache_second = cache2_second;
 	}
 
 	public void init(String region) {
-		cache1.init(region);
-		cache2.init(region);
 	}
 
 	public Object get(String key) {
-		Object obj = cache1.get(key);
+		Object obj = cache_first.get(key);
 		if (obj == null) {
-			obj = cache2.get(key);
-			cache1.put(key, obj);
+			obj = cache_second.get(key);
+			cache_first.put(key, obj);
 		}
 		return obj;
 	}
 
 	public Object get(String group, String key) {
-		Object obj = cache1.get(group ,key);
+		Object obj = cache_first.get(group ,key);
 		if (obj == null) {
-			obj = cache2.get(group ,key);
-			cache1.put(group,key, obj);
+			obj = cache_second.get(group ,key);
+			cache_first.put(group,key, obj);
 		}
 		return obj;
 	}
@@ -84,78 +78,93 @@ public class MultiCache implements Cache{
 	}
 
 	public void put(String key, Object object) {
-		cache1.put(key ,object);
-		cache2.put(key ,object);
+		cache_first.put(key ,object);
+		cache_second.put(key ,object);
 	}
 
 	public void putSafe(String key, Object object) {
-		cache1.putSafe(key ,object);
-		cache2.putSafe(key ,object);
+		cache_first.putSafe(key ,object);
+		cache_second.putSafe(key ,object);
 	}
 
 	public void put(String groupName, String key, Object object) {
-		cache1.put(groupName ,key ,object);
-		cache2.put(groupName ,key ,object);
+		cache_first.put(groupName ,key ,object);
+		cache_second.put(groupName ,key ,object);
 	}
 
 	public Set<String> getGroupKeys(String group) {
-		Set<String> keys = cache1.getGroupKeys(group);
-		if (keys == null || keys.size() == 0) {
-			return cache2.getGroupKeys(group);
+		Set<String> keys = new HashSet<String>();
+		Set<String> fkeys = cache_first.getGroupKeys(group);
+		if (fkeys != null) {
+			keys.addAll(fkeys);
 		}
-		return null;
+		Set<String> skeys = cache_second.getGroupKeys(group);
+		if (skeys != null) {
+			keys.addAll(skeys);
+		}
+		return keys;
 	}
 
 	public void cleanGroup(String group) {
-		cache1.cleanGroup(group);
-		cache2.cleanGroup(group);
+		cache_first.cleanGroup(group);
+		cache_second.cleanGroup(group);
 	}
 
 	public void clear() {
-		cache1.clear();
-		cache2.clear();
+		cache_first.clear();
+		cache_second.clear();
 	}
 
 	public void remove(String key) {
-		cache1.remove(key);
-		cache2.remove(key);
+		cache_first.remove(key);
+		cache_second.remove(key);
 	}
 
 	public void remove(String group, String key) {
-		cache1.remove(group ,key);
-		cache2.remove(group ,key);
+		cache_first.remove(group ,key);
+		cache_second.remove(group ,key);
 		
 	}
 
 	public void remove(String[] keys) {
-		cache1.remove(keys);
-		cache2.remove(keys);
+		cache_first.remove(keys);
+		cache_second.remove(keys);
 	}
 
 	public void remove(String group, String[] keys) {
-		cache1.remove(group ,keys);
-		cache2.remove(group ,keys);
+		cache_first.remove(group ,keys);
+		cache_second.remove(group ,keys);
 	}
 
 	public String getStats() {
 		StringBuffer sb = new StringBuffer();
-		sb.append("cache1:" + cache1.getStats() + "\r\n");
-		sb.append("cache2:" + cache2.getStats() + "\r\n");
+		sb.append("cache1:" + cache_first.getStats() + "\r\n");
+		sb.append("cache2:" + cache_second.getStats() + "\r\n");
 		return sb.toString();
 	}
 
+	
 	public int freeMemoryElements(int numberToFree) {
-		throw new CacheException("multicache does not support this feature.");
+		int fnum = 0;
+		int snum = 0;
+		try {
+			fnum = cache_first.freeMemoryElements(numberToFree);
+		} catch (Exception e) {
+			
+		}
+		try {
+			snum = cache_second.freeMemoryElements(numberToFree);
+		} catch (Exception e) {
+		}
+		
+		return fnum > snum ? fnum :snum;
 	}
 
 	public void destroy() {
-		cache1.destroy();
-		cache2.destroy();
-		cacheManager.removeCache(this);
 	}
 
 	public void setCacheManager(CacheManager manager) {
-		this.cacheManager = manager;
+		
 	}
 
 }
