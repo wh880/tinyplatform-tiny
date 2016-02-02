e1c8932f44f2b23b44d52a3280722ab336a9ff13
Hulk <jayzch@qq.com>
2016-02-02 15:08:19 +0800

    添加排序字段工具方法

--- db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/daosupport/TinyDslDaoSupport.java
+++ db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/daosupport/TinyDslDaoSupport.java
@@ -5,11 +5,6 @@ import org.springframework.dao.support.DaoSupport;
 import org.tinygroup.jdbctemplatedslsession.template.DslTemplate;
 import org.tinygroup.jdbctemplatedslsession.template.DslTemplateImpl;
 import org.tinygroup.tinysqldsl.DslSession;
-import org.tinygroup.tinysqldsl.Select;
-import org.tinygroup.tinysqldsl.select.OrderByElement;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -20,30 +15,6 @@ import java.util.List;
 public abstract class TinyDslDaoSupport extends DaoSupport {
 
     private DslTemplate dslTemplate;
-
-    /**
-     * 添加排序字段工具方法
-     *
-     * @param select
-     * @param orderByArray
-     * @return
-     */
-    public static Select addOrderByElements(Select select, OrderBy... orderByArray) {
-        if (orderByArray == null || select == null) {
-            return select;
-        }
-        List<OrderByElement> orderByElements = new ArrayList<OrderByElement>();
-        for (int i = 0; orderByArray[i] != null && i < orderByArray.length; i++) {
-            OrderByElement tempElement = orderByArray[i].getOrderByElement();
-            if (tempElement != null) {
-                orderByElements.add(tempElement);
-            }
-        }
-        if (orderByElements.size() > 0) {
-            select.orderBy(orderByElements.toArray(new OrderByElement[0]));
-        }
-        return select;
-    }
 
     @Override
     protected void checkDaoConfig() throws IllegalArgumentException {
