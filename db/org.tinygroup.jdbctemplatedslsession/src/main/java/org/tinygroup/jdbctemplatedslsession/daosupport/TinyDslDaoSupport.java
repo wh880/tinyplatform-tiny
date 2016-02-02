30aca770cb280e38ea5b04e5e701c70c152b9e4d
Hulk <jayzch@qq.com>
2016-02-02 14:56:38 +0800

    添加排序字段工具方法

--- db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/daosupport/TinyDslDaoSupport.java
+++ db/org.tinygroup.jdbctemplatedslsession/src/main/java/org/tinygroup/jdbctemplatedslsession/daosupport/TinyDslDaoSupport.java
@@ -5,44 +5,75 @@ import org.springframework.dao.support.DaoSupport;
 import org.tinygroup.jdbctemplatedslsession.template.DslTemplate;
 import org.tinygroup.jdbctemplatedslsession.template.DslTemplateImpl;
 import org.tinygroup.tinysqldsl.DslSession;
+import org.tinygroup.tinysqldsl.Select;
+import org.tinygroup.tinysqldsl.select.OrderByElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
- * tinydsl的dao基础类
- * @author renhui
+ * tiny DSL的dao基础类
  *
+ * @author renhui
  */
-public abstract class TinyDslDaoSupport extends DaoSupport{
-	
-	private DslTemplate dslTemplate;
+public abstract class TinyDslDaoSupport extends DaoSupport {
 
-	@Override
-	protected void checkDaoConfig() throws IllegalArgumentException {
-		if (this.dslTemplate == null) {
-			throw new IllegalArgumentException("'DslSession' or 'DslTemplate' is required");
-		}
-	}
-	@Autowired(required = false)
-	public final void setDslSession(DslSession dslSession) {
-		if (this.dslTemplate == null || dslSession != this.dslTemplate.getDslSession()) {
-			this.dslTemplate = createDslTemplate(dslSession);
-		}
-	}
-	
-	protected DslTemplate createDslTemplate(DslSession dslSession){
-		return new DslTemplateImpl(dslSession);
-	}
+    private DslTemplate dslTemplate;
 
-	public final DslTemplate getDslTemplate() {
-		return dslTemplate;
-	}
-	@Autowired(required = false)
-	public final void setDslTemplate(DslTemplate dslTemplate) {
-		this.dslTemplate = dslTemplate;
-	}
-	
-	public final DslSession getDslSession(){
-		return dslTemplate!=null?dslTemplate.getDslSession():null;
-	}
-	
+    /**
+     * 添加排序字段工具方法
+     *
+     * @param select
+     * @param orderByArray
+     * @return
+     */
+    public static Select addOrderByElements(Select select, OrderBy... orderByArray) {
+        if (orderByArray == null || select == null) {
+            return select;
+        }
+        List<OrderByElement> orderByElements = new ArrayList<OrderByElement>();
+        for (int i = 0; orderByArray[i] != null && i < orderByArray.length; i++) {
+            OrderByElement tempElement = orderByArray[i].getOrderByElement();
+            if (tempElement != null) {
+                orderByElements.add(tempElement);
+            }
+        }
+        if (orderByElements.size() > 0) {
+            select.orderBy(orderByElements.toArray(new OrderByElement[0]));
+        }
+        return select;
+    }
+
+    @Override
+    protected void checkDaoConfig() throws IllegalArgumentException {
+        if (this.dslTemplate == null) {
+            throw new IllegalArgumentException("'DslSession' or 'DslTemplate' is required");
+        }
+    }
+
+    protected DslTemplate createDslTemplate(DslSession dslSession) {
+        return new DslTemplateImpl(dslSession);
+    }
+
+    public final DslTemplate getDslTemplate() {
+        return dslTemplate;
+    }
+
+    @Autowired(required = false)
+    public final void setDslTemplate(DslTemplate dslTemplate) {
+        this.dslTemplate = dslTemplate;
+    }
+
+    public final DslSession getDslSession() {
+        return dslTemplate != null ? dslTemplate.getDslSession() : null;
+    }
+
+    @Autowired(required = false)
+    public final void setDslSession(DslSession dslSession) {
+        if (this.dslTemplate == null || dslSession != this.dslTemplate.getDslSession()) {
+            this.dslTemplate = createDslTemplate(dslSession);
+        }
+    }
+
 }
