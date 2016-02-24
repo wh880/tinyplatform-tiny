e02f30799370e7940b9647ed76638df51058a57e
305702665@qq.com <305702665@qq.com>
2016-02-24 09:14:57 +0800

    代码提交

---
+++ ext/remoteconfig/org.tinygroup.remoteconfig/src/main/java/org/tinygroup/remoteconfig/processor/RemotePropertiesFileProcessor.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.tinygroup.remoteconfig.processor;
+
+import java.util.Map;
+
+import org.tinygroup.config.util.ConfigurationUtil;
+import org.tinygroup.fileresolver.impl.AbstractFileProcessor;
+import org.tinygroup.remoteconfig.manager.ConfigItemReader;
+import org.tinygroup.vfs.FileObject;
+
+/**
+ * @author Administrator
+ *
+ */
+public class RemotePropertiesFileProcessor extends AbstractFileProcessor {
+
+	ConfigItemReader configItemReader;
+	
+	public void setConfigItemReader(ConfigItemReader configItemReader) {
+		this.configItemReader = configItemReader;
+	}
+	
+	public void process() {
+		Map<String ,String> configMap = configItemReader.getALL();
+		ConfigurationUtil.getConfigurationManager().getConfiguration().putAll(configMap);
+	}
+
+	@Override
+	protected boolean checkMatch(FileObject fileObject) {
+		return false;
+	}
+
+}
