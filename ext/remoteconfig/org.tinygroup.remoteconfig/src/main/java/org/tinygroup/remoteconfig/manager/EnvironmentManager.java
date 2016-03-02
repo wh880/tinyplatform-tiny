bbd5cc2bcbc27de1bec0fd2d0e091443bcb7aa9b
atizose <279708573@qq.com>
2016-03-02 09:07:04 +0800

    调整环境管理类接口

--- ext/remoteconfig/org.tinygroup.remoteconfig/src/main/java/org/tinygroup/remoteconfig/manager/EnvironmentManager.java
+++ ext/remoteconfig/org.tinygroup.remoteconfig/src/main/java/org/tinygroup/remoteconfig/manager/EnvironmentManager.java
@@ -9,9 +9,52 @@ public interface EnvironmentManager {
 
 	void update(Environment env, String versionId, String productId);
 
-	void delete(String envId, String versionId, String productId);
+//	void delete(String envId, String versionId, String productId);
 
 	Environment get(String envId, String versionId, String productId);
 
+	/**
+	 * 添加一个环境
+	 * @param env
+	 * @return
+	 */
+	Environment add(Environment env);
+	/**
+	 * 修改一个环境
+	 * @param env
+	 * @return
+	 */
+	void update(Environment env);
+	/**
+	 * 删除一个环境
+	 * @param env
+	 * @return
+	 */
+	void delete(String envId);
+	/**
+	 * 查询一个环境
+	 * @param env
+	 * @return
+	 */
+	Environment get(String envId);
+	/**
+	 * 关联一个环境到指定产品版本
+	 * @param env
+	 * @return
+	 */
+	void add(String envId, String versionId, String productId);
+	/**
+	 * 移联环境和指定产品版本的关联
+	 * @param env
+	 * @return
+	 */
+	void delete(String envId, String versionId, String productId);
+	
+	
 	List<Environment> query(String versionId, String productId);
+	
+	
+	
+	
+
 }
