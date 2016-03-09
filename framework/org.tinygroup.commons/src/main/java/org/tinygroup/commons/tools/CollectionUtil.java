6e819dabf5c50ee850b4b96107e5615aa149904d
Hulk <jayzch@qq.com>
2016-03-09 11:11:44 +0800

    在CollectionUtil中新增许多工具方法

--- framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/tools/CollectionUtil.java
+++ framework/org.tinygroup.commons/src/main/java/org/tinygroup/commons/tools/CollectionUtil.java
@@ -1,21 +1,26 @@
 /**
- *  Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
- *
- *  Licensed under the GPL, Version 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.gnu.org/licenses/gpl.html
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
+ * <p>
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/gpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.tinygroup.commons.tools;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.tinygroup.logger.LogLevel;
+import org.tinygroup.logger.Logger;
+import org.tinygroup.logger.LoggerFactory;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -26,6 +31,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author renhui
  */
 public final class CollectionUtil {
+    private static final Logger logger = LoggerFactory.getLogger(CollectionUtil.class);
+
     /** 创建一个<code>ArrayList</code>。 */
     public static <T> ArrayList<T> createArrayList() {
         return new ArrayList<T>();
@@ -262,34 +269,36 @@ public final class CollectionUtil {
             list.add(element);
         }
     }
-    /**
-	 * Return <code>true</code> if the supplied Collection is <code>null</code>
-	 * or empty. Otherwise, return <code>false</code>.
-	 * @param collection the Collection to check
-	 * @return whether the given Collection is empty
-	 */
-	public static boolean isEmpty(Collection collection) {
-		return (collection == null || collection.isEmpty());
-	}
-	
-	/**
-	 * Return <code>true</code> if the supplied Map is <code>null</code>
-	 * or empty. Otherwise, return <code>false</code>.
-	 * @param map the Map to check
-	 * @return whether the given Map is empty
-	 */
-	public static boolean isEmpty(Map map) {
-		return (map == null || map.isEmpty());
-	}
 
-	public static String[] toNoNullStringArray(Collection collection) {
+    /**
+     * Return <code>true</code> if the supplied Collection is <code>null</code>
+     * or empty. Otherwise, return <code>false</code>.
+     * @param collection the Collection to check
+     * @return whether the given Collection is empty
+     */
+    public static boolean isEmpty(Collection collection) {
+        return (collection == null || collection.isEmpty());
+    }
+
+    /**
+     * Return <code>true</code> if the supplied Map is <code>null</code>
+     * or empty. Otherwise, return <code>false</code>.
+     *
+     * @param map the Map to check
+     * @return whether the given Map is empty
+     */
+    public static boolean isEmpty(Map map) {
+        return (map == null || map.isEmpty());
+    }
+
+    public static String[] toNoNullStringArray(Collection collection) {
         if (collection == null) {
             return ArrayUtils.EMPTY_STRING_ARRAY;
         }
         return toNoNullStringArray(collection.toArray());
     }
-	
-	static String[] toNoNullStringArray(Object[] array) {
+
+    static String[] toNoNullStringArray(Object[] array) {
         ArrayList list = new ArrayList(array.length);
         for (int i = 0; i < array.length; i++) {
             Object e = array[i];
@@ -299,5 +308,146 @@ public final class CollectionUtil {
         }
         return (String[]) list.toArray(ArrayUtils.EMPTY_STRING_ARRAY);
     }
-	
+
+    /////////////////////////////////
+
+    /**
+     * 提取集合中的对象的两个属性(通过Getter函数), 组合成Map.
+     *
+     * @param collection        来源集合.
+     * @param keyPropertyName   要提取为Map中的Key值的属性名.
+     * @param valuePropertyName 要提取为Map中的Value值的属性名.
+     */
+    public static Map extractToMap(final Collection collection, final String keyPropertyName,
+                                   final String valuePropertyName) {
+        Map map = new HashMap(collection.size());
+        try {
+            for (Object obj : collection) {
+                map.put(PropertyUtils.getProperty(obj, keyPropertyName),
+                        PropertyUtils.getProperty(obj, valuePropertyName));
+            }
+        } catch (Exception e) {
+            logger.logMessage(LogLevel.ERROR, "转换失败", e);
+        }
+        return map;
+    }
+
+    /**
+     * 提取集合中的对象的一个属性(通过Getter函数), 组合成List.
+     *
+     * @param collection   来源集合.
+     * @param propertyName 要提取的属性名.
+     */
+    public static List extractToList(final Collection collection, final String propertyName) {
+        List list = new ArrayList(collection.size());
+        try {
+            for (Object obj : collection) {
+                list.add(PropertyUtils.getProperty(obj, propertyName));
+            }
+        } catch (Exception e) {
+            logger.logMessage(LogLevel.ERROR, "转换失败", e);
+        }
+
+        return list;
+    }
+
+    /**
+     * 提取集合中的对象的一个属性(通过Getter函数), 组合成由分割符分隔的字符串.
+     *
+     * @param collection   来源集合.
+     * @param propertyName 要提取的属性名.
+     * @param separator    分隔符.
+     */
+    public static String extractToString(final Collection collection, final String propertyName, final String separator) {
+        List list = extractToList(collection, propertyName);
+        return StringUtils.join(list, separator);
+    }
+
+    /**
+     * 转换Collection所有元素(通过toString())为String, 中间以 separator分隔。
+     */
+    public static String convertToString(final Collection collection, final String separator) {
+        return StringUtils.join(collection, separator);
+    }
+
+    /**
+     * 转换Collection所有元素(通过toString())为String, 每个元素的前面加入prefix，后面加入postfix，如<div>mymessage</div>。
+     */
+    public static String convertToString(final Collection collection, final String prefix, final String postfix) {
+        StringBuilder builder = new StringBuilder();
+        for (Object o : collection) {
+            builder.append(prefix).append(o).append(postfix);
+        }
+        return builder.toString();
+    }
+
+
+    /**
+     * 取得Collection的第一个元素，如果collection为空返回null.
+     */
+    public static <T> T getFirst(Collection<T> collection) {
+        if (isEmpty(collection)) {
+            return null;
+        }
+        return collection.iterator().next();
+    }
+
+    /**
+     * 获取Collection的最后一个元素 ，如果collection为空返回null.
+     */
+    public static <T> T getLast(Collection<T> collection) {
+        if (isEmpty(collection)) {
+            return null;
+        }
+
+        //当类型为List时，直接取得最后一个元素 。
+        if (collection instanceof List) {
+            List<T> list = (List<T>) collection;
+            return list.get(list.size() - 1);
+        }
+
+        //其他类型通过iterator滚动到最后一个元素.
+        Iterator<T> iterator = collection.iterator();
+        while (true) {
+            T current = iterator.next();
+            if (!iterator.hasNext()) {
+                return current;
+            }
+        }
+    }
+
+    /**
+     * 返回a+b的新List.
+     */
+    public static <T> List<T> union(final Collection<T> a, final Collection<T> b) {
+        List<T> result = new ArrayList<T>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    /**
+     * 返回a-b的新List.
+     */
+    public static <T> List<T> subtract(final Collection<T> a, final Collection<T> b) {
+        List<T> list = new ArrayList<T>(a);
+        for (T element : b) {
+            list.remove(element);
+        }
+        return list;
+    }
+
+    /**
+     * 返回a与b的交集的新List.
+     */
+    public static <T> List<T> intersection(Collection<T> a, Collection<T> b) {
+        List<T> list = new ArrayList<T>();
+
+        for (T element : a) {
+            if (b.contains(element)) {
+                list.add(element);
+            }
+        }
+        return list;
+    }
+
 }
