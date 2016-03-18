3875899ec4664b0d8c4251201536356e2fe4aea4
aotianji1238 <aotianji1238@163.com>
2016-03-18 09:16:51 +0800

    UI组件计算增加排序逻辑：确保在组件加载顺序不同的情况下，能返回相同的健康组件序列

--- web/org.tinygroup.uiengine/src/main/java/org/tinygroup/uiengine/manager/impl/UIComponentManagerImpl.java
+++ web/org.tinygroup.uiengine/src/main/java/org/tinygroup/uiengine/manager/impl/UIComponentManagerImpl.java
@@ -15,17 +15,22 @@
  */
 package org.tinygroup.uiengine.manager.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import org.tinygroup.commons.tools.StringUtil;
 import org.tinygroup.logger.LogLevel;
 import org.tinygroup.logger.Logger;
 import org.tinygroup.logger.LoggerFactory;
 import org.tinygroup.uiengine.config.UIComponent;
 import org.tinygroup.uiengine.config.UIComponents;
 import org.tinygroup.uiengine.manager.UIComponentManager;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class UIComponentManagerImpl implements UIComponentManager {
     static Logger logger = LoggerFactory.getLogger(UIComponentManagerImpl.class);
@@ -35,6 +40,7 @@ public class UIComponentManagerImpl implements UIComponentManager {
     private volatile boolean  calculating; //是否计算中
     private volatile boolean  modified ; //组件是否被修改
     private Map<String, UIComponent> uiMap = new HashMap<String, UIComponent>();
+    private UIComponentComparator comparator = new UIComponentComparator();//UI组件排序
 
     public List<UIComponent> getUiComponents() {
         return uiComponentList;
@@ -150,6 +156,8 @@ public class UIComponentManagerImpl implements UIComponentManager {
 			for (UIComponent component : uiComponentList) {
 				this.isHealth(component.getName());		            
 	        }
+			//排序，固定渲染顺序
+			Collections.sort(healthyComponentList,comparator);
 		}finally{
 			//计算结束，重置计算状态
 			calculating = false;
@@ -160,5 +168,138 @@ public class UIComponentManagerImpl implements UIComponentManager {
 
 	public void reset() {
 		healthyComponentList.clear();
+	}
+	
+	/**
+	 * UI组件的排序算法
+	 * @author yancheng11334
+	 *
+	 */
+	class UIComponentComparator implements Comparator<UIComponent>{
+
+		/**
+		 * UI组件关系为：前者依赖后者;后者依赖前者;两者无关系
+		 */
+		public int compare(UIComponent o1, UIComponent o2) {
+			if(isDependent(o1,o2)){
+			   return 1;
+			}else if(isDependent(o2,o1)){
+			   return -1;
+			}else{
+			   return compareByName(o1,o2);
+			}
+		}
+		
+		/**
+		 * 按组件名称进行排序
+		 * @param o1
+		 * @param o2
+		 * @return
+		 */
+		private int compareByName(UIComponent o1, UIComponent o2){
+			int v1 = getNameProtity(o1.getName());
+			int v2 = getNameProtity(o2.getName());
+			if(v1>v2){
+				return 1;
+			}else if(v1<v2){
+				return -1;
+			}else{
+				return 0;
+			}
+		}
+		
+		private int getNameProtity(String name){
+			//简单采用hashcode做优先级
+			return name.hashCode();
+		}
+		
+		/**
+		 * 前者是否继承依赖后者
+		 * @param o1
+		 * @param o2
+		 * @return
+		 */
+		private boolean isDependent(UIComponent o1, UIComponent o2) {
+			//前者没有依赖关系,直接判断false
+			if(StringUtil.isEmpty(o1.getDependencies())){
+				return false;
+			}else{
+				 Queue<String> queue = new LinkedList<String>();
+				 QueryResult result = new QueryResult(o2.getName());
+				 String[] dependencies = o1.getDependencies().split(",");
+				 for(String dependency:dependencies){
+					 //执行依赖关系搜索
+					 query(queue,dependency,result);
+				 }
+				 return result.isFind();
+			}
+		}
+		
+		/**
+		 * 查询依赖关系
+		 * @param queue
+		 * @param name
+		 * @param result
+		 */
+		private void query(Queue<String> queue,String name,QueryResult result){
+			//已经匹配，直接返回
+			if(result.isFind()){
+			   return ;
+			}else{
+				//入队操作
+				if(name!=null){
+					//如果找到目标，直接返回
+					if(result.getTarget().equals(name)){
+						result.setFind(true);
+				    	return ;
+					}else{
+						 //往队列添加不重复的元素
+						 if(!queue.contains(name)){
+				    	     queue.add(name);
+				    	 }
+					}
+				}
+				
+				//队列为空，终止查询并返回
+				if(queue.isEmpty()){
+				   return ;
+				}
+				
+				//出队操作
+				String element = queue.poll();
+				UIComponent component = uiMap.get(element);
+				if(!StringUtil.isEmpty(component.getDependencies())){
+				    String[] dependencies = component.getDependencies().split(",");
+				    for(String dependency:dependencies){
+				    	//递归调用
+				    	query(queue,dependency,result);
+				    }
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * 查询结果
+	 * @author yancheng11334
+	 *
+	 */
+	class QueryResult {
+		private String target; //目标依赖
+		private boolean find;  //是否找到
+		public QueryResult(String target){
+			this.target = target;
+		}
+		public String getTarget() {
+			return target;
+		}
+		public boolean isFind() {
+			return find;
+		}
+		public void setFind(boolean find) {
+			this.find = find;
+		}
+		
 	}
 }
---
+++ web/org.tinygroup.uiengine/src/test/java/org/tinygroup/uiengine/TestSort.java
@@ -0,0 +1,89 @@
+package org.tinygroup.uiengine;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import org.tinygroup.uiengine.config.UIComponent;
+import org.tinygroup.uiengine.config.UIComponents;
+import org.tinygroup.uiengine.manager.UIComponentManager;
+import org.tinygroup.uiengine.manager.impl.UIComponentManagerImpl;
+
+/**
+ * UI组件排序类
+ * @author yancheng11334
+ *
+ */
+public class TestSort extends TestCase {
+
+	private UIComponent u1;
+	private UIComponent u2;
+	private UIComponent u3;
+	private UIComponent u4;
+	private UIComponent u5;
+	private UIComponent u6;
+	
+	UIComponentManager manager;
+	
+	protected void setUp(){
+		u1 = new UIComponent();
+		u1.setName("u1");
+		u1.setDependencies("u2,u3");
+		
+		u2 = new UIComponent();
+		u2.setName("u2");
+		
+		u3 = new UIComponent();
+		u3.setName("u3");
+		u3.setDependencies("u4");
+		
+		u4 = new UIComponent();
+		u4.setName("u4");
+		
+		u5 = new UIComponent();
+		u5.setName("u5");
+		u5.setDependencies("u6");
+		
+		u6 = new UIComponent();
+		u6.setName("u6");
+	}
+	
+	private UIComponents createUIComponent(UIComponent... list){
+		UIComponents components = new UIComponents();
+		if(list!=null){
+			ArrayList<UIComponent> newlist = new ArrayList<UIComponent>();
+			for(UIComponent component:list){
+				newlist.add(component);
+			}
+			components.setComponents(newlist);
+		}
+		return components;
+	}
+	
+	/**
+	 * 指定不同的顺序，计算完毕顺序固定
+	 */
+	public void testOrder(){
+	    manager = new UIComponentManagerImpl();
+		manager.addUIComponents(createUIComponent(u1,u2,u3,u4,u5,u6));
+		manager.compute();
+		
+		assertEquals(u2, manager.getHealthUiComponents().get(0));
+		assertEquals(u4, manager.getHealthUiComponents().get(1));
+		assertEquals(u3, manager.getHealthUiComponents().get(2));
+		assertEquals(u1, manager.getHealthUiComponents().get(3));
+		assertEquals(u6, manager.getHealthUiComponents().get(4));
+		assertEquals(u5, manager.getHealthUiComponents().get(5));
+		
+		manager = new UIComponentManagerImpl();
+		manager.addUIComponents(createUIComponent(u6,u5,u4,u3,u2,u1));
+		manager.compute();
+		
+		assertEquals(u2, manager.getHealthUiComponents().get(0));
+		assertEquals(u4, manager.getHealthUiComponents().get(1));
+		assertEquals(u3, manager.getHealthUiComponents().get(2));
+		assertEquals(u1, manager.getHealthUiComponents().get(3));
+		assertEquals(u6, manager.getHealthUiComponents().get(4));
+		assertEquals(u5, manager.getHealthUiComponents().get(5));
+	}
+}
