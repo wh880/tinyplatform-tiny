d6da5b32ba82f8b8a259b959464a80f0f11e9f6c
atizose <279708573@qq.com>
2016-03-18 16:43:21 +0800

    调整代码，使得服务端发生异常后，客户端可以获取到异常信息，而非之前仅仅提示"http调用失败"

--- web/org.tinygroup.servicehttpchannelweb/src/main/java/org/tinygroup/servicehttpchannelweb/ServiceHttpServiceProcessor.java
+++ web/org.tinygroup.servicehttpchannelweb/src/main/java/org/tinygroup/servicehttpchannelweb/ServiceHttpServiceProcessor.java
@@ -1,20 +1,21 @@
 package org.tinygroup.servicehttpchannelweb;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 
 import org.tinygroup.cepcore.CEPCore;
 import org.tinygroup.context.Context;
 import org.tinygroup.event.Event;
-import org.tinygroup.event.Parameter;
-import org.tinygroup.event.ServiceInfo;
+import org.tinygroup.logger.Logger;
+import org.tinygroup.logger.LoggerFactory;
 import org.tinygroup.servicehttpchannel.Hession;
 import org.tinygroup.weblayer.AbstractTinyProcessor;
 import org.tinygroup.weblayer.WebContext;
 
 public class ServiceHttpServiceProcessor extends AbstractTinyProcessor {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ServiceHttpServiceProcessor.class);
 	CEPCore core;
 	
 	@Override
@@ -28,29 +29,27 @@ public class ServiceHttpServiceProcessor extends AbstractTinyProcessor {
 		int lastSplash = urlString.lastIndexOf('/');
 		int lastDot = urlString.lastIndexOf('.');
 		String serviceId = urlString.substring(lastSplash + 1, lastDot);
-		Object result = callService(serviceId, context);
-		if (urlString.endsWith("mockservice") && result != null) {// 返回xml
-			context.getResponse().getOutputStream().write(Hession.serialize(result));
+		Event event = callService(serviceId, context);
+		event.setType(Event.EVENT_TYPE_RESPONSE);
+		if (urlString.endsWith("mockservice") ) {// 返回xml
+			context.getResponse().getOutputStream().write(Hession.serialize(event));
 		}
 	}
 
-	private Object callService(String serviceId, Context context) {
-		// CEPCore core = SpringBeanContainer.getBean(CEPCore.CEP_CORE_BEAN);
-		
+	private Event callService(String serviceId, Context context) {
 		Event event = null;
 		try {
 			event = (Event) Hession.deserialize( (byte[]) context.get("TINY_MOCK_SERVICE"));
-			core.process(event);
 		} catch (IOException e1) {
-			throw new RuntimeException("请求对象反序列化失败",e1);
+			throw new RuntimeException("请求:"+serviceId+"对象反序列化失败",e1);
 		}
-		ServiceInfo info = core.getServiceInfo(serviceId);
-		List<Parameter> resultsParam = info.getResults();
-		if (resultsParam == null || resultsParam.size() == 0) {
-			return null;
+		try {
+			core.process(event);
+		} catch (Exception e1) {
+			LOGGER.errorMessage("请求:{}执行异常", e1,serviceId);
+			event.setThrowable(e1);
 		}
-		return event.getServiceRequest().getContext()
-				.get(resultsParam.get(0).getName());
+		return event;
 	}
 
 	public CEPCore getCore() {
