0442371bd65a0d28f359bc803820020b08f18bc4
ballackhui <ballackhui@gmail.com>
2016-06-17 11:32:42 +0800

    taskId=429,roxyFactoryBeanDefineCreate实现增加是否是接口判断

--- ext/org.tinygroup.custombeandefine/src/test/java/org/tinygroup/custombeandefine/service/impl/MySayHelloService.java
+++ ext/org.tinygroup.custombeandefine/src/test/java/org/tinygroup/custombeandefine/service/impl/MySayHelloService.java
@@ -2,7 +2,7 @@ package org.tinygroup.custombeandefine.service.impl;
 
 import org.tinygroup.custombeandefine.service.SayHelloService;
 
-public class SayHelloImpl implements SayHelloService {
+public class MySayHelloService implements SayHelloService {
 
 	public String sayHello() {
 		return "hello everyone";
