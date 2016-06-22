bd588596472ec56ba3947e55a48f1629a6ad93dc
ballackhui <ballackhui@gmail.com>
2016-06-22 09:36:14 +0800

    添加注释

--- db/org.tinygroup.sequence/src/main/java/org/tinygroup/sequence/DataSourceSelector.java
+++ db/org.tinygroup.sequence/src/main/java/org/tinygroup/sequence/DataSourceSelector.java
@@ -9,6 +9,11 @@ import java.util.List;
  */
 public interface DataSourceSelector {
 
+	/**
+	 * 随机选择数据源的接口，
+	 * @param excludeIndexes 排除的数据源序号
+	 * @return 返回数据源序号
+	 */
 	public int getRandomDataSourceIndex(List<Integer> excludeIndexes);
 	
 }
